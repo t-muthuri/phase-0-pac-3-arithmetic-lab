dd244e46b9ba1b684faffdee51e934d4f27c3e8c
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-09 09:39:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+let a=995;
+let b=5;
+
+function add (a, b){};
+function subtract (a, b){};
+function multiply (a, b){};
+function divide (a, b){};
+function add (a, b){};
+function add (a, b){
+    return (a+b);
+}
+function subtract (a, b){
+    return (a-b);
+}
+function multiply (a, b){
+    return (a*b);
+}
+function divide(a, b){
+    return (a/b);
+}
+
+let n = 654
+function increment (n){
+    return (n+1);
+}
+
+function decrement (n){
+    return (n-1);
+}
+
+function makeInt (n){
+    return parseInt (n, 10);
+}
+
+function preserveDecimal (n){
+    return parseFloat (n);
+}
